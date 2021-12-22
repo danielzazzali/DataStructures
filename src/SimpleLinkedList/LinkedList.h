264ecc6ba9821a8a3c881f0f180d96d153ad0125
daniel <dazazzali@gmail.com>
2021-12-22 14:57:55 -0300

    Added LinkedList .cpp and .h files

---
+++ src/SimpleLinkedList/LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by daniel on 22-12-21.
+//
+
+#ifndef DATASTRUCTURES_LINKEDLIST_H
+#define DATASTRUCTURES_LINKEDLIST_H
+
+
+class LinkedList {
+
+};
+
+
+#endif //DATASTRUCTURES_LINKEDLIST_H
